52fa8cff0e6f340dcdebd5848fd1bd1d25a22b59
Amine B. Hassouna <amine.benhassouna@gmail.com>
2018-12-02 11:33:26 +0100

    Align center window and remove old code

--- src/main.c
+++ src/main.c
@@ -63,8 +63,8 @@ int main(int argc, char* argv[])
 
     // Create window
     SDL_Window *window = SDL_CreateWindow("Flying plane SDL animation",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
                                           SCREEN_WIDTH, SCREEN_HEIGHT,
                                           SDL_WINDOW_SHOWN);
     if(!window)
@@ -83,50 +83,6 @@ int main(int argc, char* argv[])
         }
         else
         {
-            // Declare rect of square
-            SDL_Rect squareRect;
-
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-            squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-            // Square position: In the middle of the screen
-            squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-            squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
-
-            // Event loop exit flag
-            bool quit = false;
-
-            // Event loop
-            while(!quit)
-            {
-                SDL_Event e;
-
-                // Wait indefinitely for the next available event
-                SDL_WaitEvent(&e);
-
-                // User requests quit
-                if(e.type == SDL_QUIT)
-                {
-                    quit = true;
-                }
-
-                // Initialize renderer color white for the background
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-                // Clear screen
-                SDL_RenderClear(renderer);
-
-                // Set renderer color red to draw the square
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-
-                // Draw filled square
-                SDL_RenderFillRect(renderer, &squareRect);
-
-                // Update screen
-                SDL_RenderPresent(renderer);
-            }
 
             // Destroy renderer
             SDL_DestroyRenderer(renderer);
