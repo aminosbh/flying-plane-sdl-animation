08492936f317a663b94928ff7959881138dd27f3
Amine B. Hassouna <amine.benhassouna@gmail.com>
2020-02-23 20:38:57 +0100

    Fix included headers

--- include/utils.h
+++ include/utils.h
@@ -34,9 +34,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL2_framerate.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL2_framerate.h>
 
 // Define MAX and MIN macros
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
